cbdf86122447ab1da2a460f4ac902383ef158415
Vijay Kumar <118923157+vijayvrx8055@users.noreply.github.com>
2024-06-14 02:19:51 +0530

    inital config files

---
+++ currency-config/src/test/java/com/vrx/currency/config/CurrencyConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrx.currency.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
